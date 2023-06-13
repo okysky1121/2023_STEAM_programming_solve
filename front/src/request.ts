aea8403a9b16ce2532cc087f55217bc1999e485d
okysky1121 <58683489+okysky1121@users.noreply.github.com>
2023-06-13 04:42:31 +0000

    API 요청 추가

---
+++ front/src/request.ts
@@ -0,0 +1,26 @@
+import qs from "query-string";
+
+type Location = {};
+type LocationResponse = { result: Location[] };
+
+const BASE_URL = process.env.API_URL || "http://localhost:8000";
+
+const request = (path: string, query: object) => {
+    return fetch(`${BASE_URL}${path}${qs.stringify(query)}`)
+};
+
+const requestSearch = async (q: string) => {
+    const res = await request("/search", { q });
+    const results: LocationResponse = await res.json()
+
+    return results.result;
+};
+
+const requestNearby = async (lat: number, lon: number) => {
+    const res = await request("/nearby", { lat, lon });
+    const results: LocationResponse = await res.json();
+
+    return results.result;
+};
+
+export { requestSearch, requestNearby };
